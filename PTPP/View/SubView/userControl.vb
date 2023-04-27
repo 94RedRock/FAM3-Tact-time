6e8bee4ff49abdba6eb60b7a5e5631ce4f903f6e
45370015 <kyungmin.chung@ymfk.yokogawa.com>
2023-04-27 17:09:32 +0900

    suffix DB -comparedata 까지 성공

--- PTPP/View/SubView/userControl.vb
+++ PTPP/View/SubView/userControl.vb
@@ -900,4 +900,8 @@ Public Class userControl
     Private Sub txtSearch_TextChanged(sender As Object, e As EventArgs) Handles txtSearch.TextChanged
 
     End Sub
+
+    Private Sub Label12_Click(sender As Object, e As EventArgs) Handles Label12.Click
+
+    End Sub
 End Class
